32d41e4e185ffa721726eebc3cfdeab4a80fc4a0
Vitaliy <nezhinskiy22@gmail.com>
2022-05-13 21:39:53 +0300

    ref() issue

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,10 +1,39 @@
-import React from "react";
-import { createContext } from "react/cjs/react.development";
-import { useContext, useState } from "react/cjs/react.production.min";
+import React, { createContext, useContext, useState } from "react";
 
 const ResultContext = createContext();
 const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("Kamyanets-Podilskiy");
+
+  const getResults = async (type) => {
+    setIsLoading(true);
+
+    const response = await fetch(`${baseUrl}${type}`, {
+      method: "GET",
+      headers: {
+        "X-User-Agent": "desktop",
+        "X-Proxy-Location": "EU",
+        "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+        "X-RapidAPI-Key": "568f93900amsh70cb898ae51211ap19e818jsn36e1d7d408d7",
+      },
+    });
+
+    const data = await response.json();
+
+    setResults(data);
+    setIsLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
 };
+
+export const useResultContext = () => useContext(ResultContext);
